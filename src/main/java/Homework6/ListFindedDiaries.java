e6ad679e5438867c2de6f4be8c5105e18b6ab0a0
ViktoriA <viktoriya1008@gmail.com>
2022-05-24 15:19:48 +0700

    Homework6-done

---
+++ src/main/java/Homework6/ListFindedDiaries.java
@@ -0,0 +1,22 @@
+package Homework6;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class ListFindedDiaries extends BaseView {
+
+    public ListFindedDiaries(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//div[@class='item' and not(.//span[@class='i-lock'])]/div[@class='col-sm-2 no-gutter']")
+    public List<WebElement> listNotPrivateDiaries;
+
+    public DiaryPage goToFirstNotPrivateDiary() {
+        listNotPrivateDiaries.stream().findFirst().get().click();
+        return new DiaryPage(driver);
+    }
+}
